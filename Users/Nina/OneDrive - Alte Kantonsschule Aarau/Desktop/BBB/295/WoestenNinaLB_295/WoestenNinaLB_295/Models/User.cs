550b9224e7701026bfa0669518f1f85a5ee5594a
Nina Woesten <105288781+NinaWoesten@users.noreply.github.com>
2023-11-01 10:06:26 +0100

    Delete Users/Nina/OneDrive - Alte Kantonsschule Aarau/Desktop/BBB/295/WoestenNinaLB_295 directory

--- Users/Nina/OneDrive - Alte Kantonsschule Aarau/Desktop/BBB/295/WoestenNinaLB_295/WoestenNinaLB_295/Models/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WoestenNinaLB_295.Models
-{
-    public class User
-    {
-        public  string Username { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty ;
-    }
-}
